216d2402f1efe3e1f6efdbd6bfe263803f331e89
triyasniko <triyasniko@gmail.com>
2022-04-01 17:07:42 +0700

    add let script

--- latihan2.js
+++ latihan2.js
@@ -13,12 +13,56 @@
 // i=10;
 // console.log(i);
 
-function luasPersegi(sisi){
-    var luas=sisi*sisi;
-    // console.log(luas);
-    return 'Hello';
-}
+// function luasPersegi(sisi){
+//     var luas=sisi*sisi;
+//     // console.log(luas);
+//     return 'Hello';
+// }
 
-// luasPersegi();
-console.log(luasPersegi(8));
-luasPersegi(10);
+// // luasPersegi();
+// console.log(luasPersegi(8));
+// luasPersegi(10);
+
+// var tester="hei hi";
+// function newFunction(){
+//     var hello ="hello";
+//     return hello;
+// }
+
+// // console.log(hello);
+// console.log(newFunction());
+
+// var greeter="hey hi";
+// greeter="say Hello instead";
+
+// console.log(greeter);
+
+// var greeter;
+// console.log(greeter); // greeter is undefined
+// greeter = "say hello"
+
+// var greeter="hey hi";
+// var times=2;
+
+// if(times>3){
+//     var greeter="say Hello instead";
+// }
+
+// console.log(greeter)
+
+let greeting = "Hi";
+let times = 4;
+let name="Rainy";
+let hello='halo';
+
+if (times > 3) {
+    // function abc(){
+    //     let hello = "Hello";
+    //     console.log(hello+greeting);// "say Hello instead"
+    // }
+    let hello = "Hello";
+    console.log(hello);
+}
+// console.log(hello) // hello is not defined
+// abc();
+console.log(hello+name);
