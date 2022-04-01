0b015049bfbb3823895b2ed047f066f090d18424
triyasniko <triyasniko@gmail.com>
2022-04-01 16:11:45 +0700

    function

---
+++ latihan2.js
@@ -0,0 +1,24 @@
+// perulangan bilangan prima 1-50
+// for(i=1;i<=5;i++){
+//     console.log(i);
+//     let a=0;
+    // for(let j=1;j<=i;j++){
+    //     console.log("J:"+j);
+    //     if(i%j==0){
+    //         a++;
+    //     }
+    // }
+// }
+// let i=0;
+// i=10;
+// console.log(i);
+
+function luasPersegi(sisi){
+    var luas=sisi*sisi;
+    // console.log(luas);
+    return 'Hello';
+}
+
+// luasPersegi();
+console.log(luasPersegi(8));
+luasPersegi(10);
